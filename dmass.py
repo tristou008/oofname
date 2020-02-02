36e5e474a49d61696a52ce60609cfb799a4e9261
tristou008 <60561095+tristou008@users.noreply.github.com>
2020-02-02 04:24:30 +0100

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -8,7 +8,7 @@ import colorsys
 import random
 import time
 
-client = commands.Bot(command_prefix = '!!', case_insensitive=True)
+client = commands.Bot(command_prefix = '=', case_insensitive=True)
 Client = discord.client
 Clientdiscord = discord.Client()
 
@@ -44,4 +44,4 @@ async def send(ctx, *, content: str):
                 await client.say("DM can't Sent To : {} :x: ".format(member))
 
 
-client.run("NjczMzYyMTg2NzY4MzUxMjQy.XjY7iQ.WCTT6XdwDw_xUyJnXBoQMeWf6IM")                
+client.run("NjczMzY3ODU2ODkyODcwNjY2.XjZA3A.VIjz2WnuZYGcxtsHH-f36GWV_jc")                
